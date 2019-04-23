e45fda467c51edaa7704a39e57e59301cccef99b
Arjun Modi <mrarjunmodi@gmail.com>
2019-04-23 12:16:35 +0100

    added Hero page created basic navbar

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-console.log('working')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { HashRouter as Router } from 'react-router-dom'
+
+import 'bulma'
+import './style.scss'
+
+import Hero from './components/Hero'
+//import Bikes from './components/Bikes'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+  }
+
+  render() {
+    return (
+      <Router>
+        <main>
+
+          <Hero />
+        </main>
+      </Router>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return(
+    <section id="hero">
+      <div id="hero-overlay">
+        <h1 className="title is-1 is-centered">Should I cycle today?</h1>
+        <div className="buttons is-centered">
+          <div className="button">Weather</div>
+          <div className="button">Bike Docks</div>
+          <div className="button">Air Quality</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
