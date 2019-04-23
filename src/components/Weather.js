060fbdb8acb4356d6c1ea1a2adb399c81c6a8952
Arjun Modi <mrarjunmodi@gmail.com>
2019-04-23 14:43:54 +0100

    added unix time from weather api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -13,7 +13,32 @@ class Weather extends React.Component {
 
   componentDidMount() {
     axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/edd734530cf0d4569f24705b5e593c4a/51.509865,-0.118092')
-      .then(res => this.setState({ weather: res.data}))
+    //.then(res => this.setState({ weather: res.data}))
+
+      .then(res => {
+        let presentState = {...this.state}
+        presentState.currentWeather = res.data.minutely.summary
+        presentState.time = res.data.minutely.data[0].time
+        presentState.icon = res.data.hourly.data[0].icon
+        console.log(presentState.time)
+        console.log(presentState.currentWeather)
+        console.log(presentState.icon)
+
+        this.setState({
+          ...presentState
+        })
+
+        function unixTimestamp(t) {
+          let dt = new Date(t*1000)
+          let hr = dt.getHours()
+          let m = '0' + dt.getMinutes()
+          let s = '0' + dt.getSeconds()
+          return hr+ ':' + m.substr(-2) + ':' + s.substr(-2)
+        }
+
+        console.log(unixTimestamp(presentState.time))
+
+      })
   }
 
 
@@ -21,7 +46,7 @@ class Weather extends React.Component {
     return(
       <section className="section">
         <div className="container">
-          <h1 className="title is-1">{this.state.weather.timezone}</h1>
+          <h1 className="title is-1">{this.unixTimestamp.bind(this)}</h1>
 
         </div>
       </section>
